7b341595e2d67d6afbbf3a93eaec657e01fa9669
amalia <amalia.reodica@gmail.com>
2022-04-04 16:00:13 -0400

    starting project for a card game called Sequence, set up card api

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [players, setPlayers] = useState(1)
+  const [deck, setDeck] = useState('')
+  const [hands, setHands] = useState()
+  let huh 
+  const newDeck = () => {
+    const numberOfDecks = 2
+    const url = `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=${numberOfDecks}`
+    fetch(url)
+      .then(res => res.json())
+      .then(decks => {
+        setDeck(decks.deck_id)
+        console.log(decks.deck_id)
+      })
+  }
+
+  const newHands = () => {
+    const numberOfCards = players * 5
+    const url = `https://deckofcardsapi.com/api/deck/${deck}/draw/?count=${numberOfCards}`
+    fetch(url)
+      .then(res => res.json())
+      .then(drawnHands => {
+        const handsDrawn = drawnHands.cards
+        console.log(handsDrawn)
+        let cardElements = handsDrawn.map((card: { code: string | null | undefined; image: string | undefined; value: number; suit: string; }) => (
+            <img key={card.code} className="handCard" src={card.image} alt={`${card.value} of ${card.suit}`} />
+        ))
+        console.log(cardElements)
+        setHands(cardElements)
+
+      })
+  }
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Sequence Game</h1>
+        <button onClick={newDeck}>New Deck</button>
+        <button onClick={newHands}>New Hands</button>
+        <div className="drawn-hand"> {hands} </div>
+       
       </header>
     </div>
   );
